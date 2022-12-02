97a98ce0e28e0017e3544cc0818575f6b10e1672
Kuber Sharma <kubersharma002@gmail.com>
2022-12-02 14:45:47 +0530

    Some Changes in requirement and setup file

--- setup.py
+++ setup.py
@@ -1,20 +1,27 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages,setup
+
 from typing import List
 
 REQUIREMENT_FILE_NAME="requirements.txt"
+HYPHEN_E_DOT = "-e ."
 
-def get_requirements() ->List[str]:
+def get_requirements()->List[str]:
+    
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        requirement_List = requirement_file
-        requirement_List = 
+        requirement_list = requirement_file.readlines()
+    requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+    
+    if HYPHEN_E_DOT in requirement_list:
+        requirement_list.remove(HYPHEN_E_DOT)
+    return requirement_list
+
+
 
 setup(
     name="sensor",
-    version="0.0.1",
+    version="0.0.2",
     author="ineuron",
-    author_email="kubersharma002@gmail.com",
-    packages=find_packages(),
+    author_email="avnish@ineuron.ai",
+    packages = find_packages(),
     install_requires=get_requirements(),
-
-
 )
